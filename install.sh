433b63255586d7a6b5ed358740e0493d55ca88a5
yingshaoxo <yingshaoxo@gmail.com>
2023-07-15 18:26:58 +0800

    fix the pip install error

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@ mkdir -p ~/.vim/pack
 cd ~/.vim/pack
 git clone https://github.com/yingshaoxo/yingshaoxo_code_pilot.git
 
-#pip install auto_everything -y
-
 curl -sSL https://bootstrap.pypa.io/get-pip.py | python
-pip install "git+https://github.com/yingshaoxo/auto_everything.git@dev" -y
+curl -sSL https://bootstrap.pypa.io/get-pip.py | python3
+
+#pip install auto_everything -y
+pip install "git+https://github.com/yingshaoxo/auto_everything.git@dev"
